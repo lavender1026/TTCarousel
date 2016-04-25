53a9b20267f4906bfb9517ae679cbdb8b04d9040
lavender <lavender@example.com>
2016-04-26 00:02:40 +0800

    单独编程
    貌似写错了一些

---
+++ TTCarousel/TTCarousel/TTImageCell.m
@@ -0,0 +1,13 @@
+//
+//  TTImageCell.m
+//  图片轮播器手动版
+//
+//  Created by Lavender on 16/3/18.
+//  Copyright © 2016年 lavender. All rights reserved.
+//
+
+#import "TTImageCell.h"
+
+@implementation TTImageCell
+
+@end
---
+++ TTCarousel/TTCarousel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  图片轮播器手动版
+//
+//  Created by Lavender on 16/3/18.
+//  Copyright © 2016年 lavender. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UICollectionViewController
+
+
+@end
+
